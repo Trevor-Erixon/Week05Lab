1349b105193d25693bb9dda8b0128d8ad8471eab
Trevor-Erixon <Trevor-Erixon>
2021-02-08 12:11:03 -0700

    Additional code updates Added to implementation of login method in AccountService class Added to implementation of doPost method in LoginServlet

--- src/java/models/AccountService.java
+++ src/java/models/AccountService.java
@@ -6,12 +6,24 @@ package models;
  */
 public class AccountService {
     
+    private String username_1 = "Abe";
+    private String username_2 = "Barb";
+    
     public AccountService() {
         
     }
     
-    public User login(String username, String password) {        
-        User user = new User();
+    public User login(String username, String password) {
+        User user = null;
+        
+        if (username.equalsIgnoreCase(username_1) || username.equalsIgnoreCase(username_2))
+        {
+            if (password.equals("password"))
+            {
+                user = new User(username, null);
+            }
+        }
+        
         return user;
     }
     
--- src/java/servlets/LoginServlet.java
+++ src/java/servlets/LoginServlet.java
@@ -6,6 +6,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.AccountService;
+import models.User;
 
 /**
  *
@@ -24,6 +26,18 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        AccountService accServ = new AccountService();
+        
+        User user = accServ.login(username, password);      
+        
+        if (user == null)
+        {
+            
+        }
         
     }
 
